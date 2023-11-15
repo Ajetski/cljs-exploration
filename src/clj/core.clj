6e5110066cb5254e9096bce5e455898d2723fa5a
Adam Jeniski <adam.jeniski@flashtalking.com>
2023-11-15 13:36:06 -0500

    add run task

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1,23 +1,17 @@
-(ns core
+(ns clj.core
   (:require
    [ring.adapter.jetty :refer [run-jetty]]
    [ring.util.response :refer [redirect]]
    [compojure.core :refer [defroutes GET]]
    [compojure.route :as route]))
 
-; for development, eval file to restart server
-(when (resolve 'server)
-  (declare server)
-  (.stop server))
-
-(defn get-cwd []
-  (-> (java.io.File. ".") .getAbsolutePath))
+(def cwd (-> (java.io.File. ".") .getAbsolutePath))
 
 (defroutes app
   (GET "/" [] (redirect "/assets/index.html"))
   (route/resources "/assets")
-  (route/files "/assets/cljs" {:root (str (get-cwd) "/src/cljs/")})
-  (route/files "/assets/cljc" {:root (str (get-cwd) "/src/cljc/")})
+  (route/files "/assets/cljs" {:root (str cwd "/src/cljs/")})
+  (route/files "/assets/cljc" {:root (str cwd "/src/cljc/")})
   (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main
@@ -25,5 +19,9 @@
   (run-jetty app {:port 3000
                   :join? false}))
 
-(def server (-main))
-
+(comment
+  (do
+    ; for development, eval file to restart server
+    (when (resolve 'server)
+      (.stop (resolve 'server)))
+    (def server (-main))))
